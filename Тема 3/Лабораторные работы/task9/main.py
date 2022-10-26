463528a28a410547b1d82db27225d8f47cb94cc0
Asy26 <Ascanow96@mail.ru>
2022-10-26 18:25:28 +0300

    lab 26.10

---
+++ Тема 3/Лабораторные работы/task9/main.py
@@ -0,0 +1,14 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+summ = spend
+need_money = 0  # количество денег, чтобы прожить 10 месяцев
+
+all_sum_salary = salary * 10  # заработная плата за 10 месяцев
+for i in range(2, months + 1):
+    spend += spend * increase
+    summ += spend
+
+need_money = summ - all_sum_salary
+print(round(need_money))
