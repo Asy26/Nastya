a354d30d13f49e9ed5d37f6e07f51ff9c5aea6f1
Анастасия <115359141+Asy26@users.noreply.github.com>
2022-10-30 20:44:15 +0300

    Update and rename Лабораторные работы/task1/main.py to Лабораторные работы/задача 1/main.py

--- Лабораторные работы/задача 1/main.py
+++ Лабораторные работы/задача 1/main.py
@@ -1,6 +1,6 @@
 src = not False and True or False and not True
 
-result = True and True or False and True
+result = True and True or False and False
 result = True or False
 result = True
 # TODO расписать упрощение выражения
